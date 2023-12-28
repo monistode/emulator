fb29919535cd84cb152e7e1df3f2ac904f7a3018
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-12-28 10:45:53 +0200

    Fix harvard reading

--- src/executable/executable.rs
+++ src/executable/executable.rs
@@ -16,7 +16,7 @@ impl Executable {
     pub fn new(bytes: &[u8]) -> Executable {
         Executable {
             header: ExecutableHeader {
-                harvard: bytes[0] == 0,
+                harvard: bytes[0] != 0,
                 entry_point: u32::from_le_bytes([bytes[1], bytes[2], bytes[3], bytes[4]]),
             },
             segments: ExecutableSegments::new(&bytes[5..]),
